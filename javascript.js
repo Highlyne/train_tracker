479e7eee7b724b859c82c0235b83c49cd554733d
Tamara Hall <tamarahall@Tamaras-MBP.lan>
2017-12-18 22:29:58 -0500

    buttons are pulling data to new var

--- javascript.js
+++ javascript.js
@@ -1,24 +1,29 @@
 console.log("this is connected");
 
-$("#btn-submit").on("click", function(event) {
-    event.preventDefault();
-    console.log("button worked");
-});
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCBu27Q_2OJZZHRs4z_9CQUdy5pgf7i20U",
+    authDomain: "train-tracker-554a5.firebaseapp.com",
+    databaseURL: "https://train-tracker-554a5.firebaseio.com",
+    projectId: "train-tracker-554a5",
+    storageBucket: "train-tracker-554a5.appspot.com",
+    messagingSenderId: "199774892630"
+  };
 
+  firebase.initializeApp(config);
 
-//   // Initialize Firebase
-//   var config = {
-//     apiKey: "AIzaSyCBu27Q_2OJZZHRs4z_9CQUdy5pgf7i20U",
-//     authDomain: "train-tracker-554a5.firebaseapp.com",
-//     databaseURL: "https://train-tracker-554a5.firebaseio.com",
-//     projectId: "train-tracker-554a5",
-//     storageBucket: "train-tracker-554a5.appspot.com",
-//     messagingSenderId: "199774892630"
-//   };
-//   firebase.initializeApp(config);
+  var database= firebase.database();
 
-//   var database= firebase.database();
+  var name ="";
+  var departure ="";
+  var arrival = "";
 
-//   $("#submit").on("click", function() {
-//       console.log("this works");
-//   })
+  $("#btn-submit").on("click", function(event) {
+      event.preventDefault();
+      console.log("this works" + name);
+      name = $("#trainName").val()
+      departure = $("#depLocation").val();
+      arrival = $("#arrLocation").val();
+      console.log("this works" + departure);
+      console.log("this works" + arrival);
+  })
